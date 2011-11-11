16cda01d67e9273f3ec660029f9754720d52ea12
Luke BrownGold <ma@gmail.com>
2011-11-10 23:24:21 -0500

    Test adding stuff to profiles

--- application/models/tank_auth/users.php
+++ application/models/tank_auth/users.php
@@ -378,6 +378,7 @@ class Users extends CI_Model
 	private function create_profile($user_id)
 	{
 		$this->db->set('user_id', $user_id);
+		$this->db->set('website', 'test');
 		return $this->db->insert($this->profile_table_name);
 	}
 
