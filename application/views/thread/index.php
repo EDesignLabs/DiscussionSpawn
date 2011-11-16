18b574a3c032eef3098fa94de1cc9689b5e3c960
Luke BrownGold <ma@gmail.com>
2011-11-15 23:00:50 -0500

    Code reorganization

--- application/views/thread/index.php
+++ application/views/thread/index.php
@@ -11,146 +11,155 @@
             <?php $this->load->view('thread/menu');?>
 			
 			<script type="text/javascript">
-				$(function() {
-					var pageHeight = 100;
-				
-					$('.node').each(function(index) {						
-						if ($(this).offset().top > pageHeight )
-							pageHeight = $(this).offset().top;
-					});
-				
-				   $('#content').animate({
-					height: pageHeight
-				  }, 0, function() {
-					// Animation complete.
-				  });
-				});
-				
-				$(document).ready(function() {
-					
-					$("a#inline").fancybox({
-						'hideOnContentClick': false
-					});
-					
-				});
-				
 				$( init );
 				 
 				function init() {
-				  var mousePos = 0;
 				
-				  $('.node').draggable({ 
-				  grid: [ 5,5 ],
-				  axis: 'y' ,
-				  stop: function(event, ui) { 
-								$(function() {
-									var pageHeight = 100;
+					//intialize variables
+					var mousePosX = 0;
+					var mouseStartDragPos = 0;
+					var isAnimating = false;
+					var nodeOptions = { 
+						grid: [ 5,5 ],
+						axis: 'y' ,
+						start:function(event, ui) {
+						mouseStartDragPos = mousePosX;
+						},
+						
+						stop: function(event, ui) { 
+										var pageHeight = 0;
+									
+										$('.node').each(function(index) {						
+											if ($(this).offset().top > pageHeight )
+												pageHeight = $(this).offset().top;
+										});
+									
+									   $('#content-nodes').css('height', pageHeight+"px" );
+						},
+						drag:function(event, ui) {
+							var that = $(this);
+							var currentPos = $(this).data('position');
+							
+							if (isAnimating == false && mousePosX > 0 && Math.abs(mousePosX-mouseStartDragPos) > 75){
+
+								if (mousePosX > 0 &&  mousePosX < 475){
+									var addClass = 'left';
+								}else if (mousePosX >= 475 &&  mousePosX < 550){ 
+									var addClass = 'full';
+								}else if (mousePosX >= 550 &&  mousePosX < 1000){ 
+									var addClass = 'right';
+								}
 								
-									$('.node').each(function(index) {						
-										if ($(this).offset().top > pageHeight )
-											pageHeight = $(this).offset().top;
+								
+								if (currentPos != addClass){
+									isAnimating = true;
+									$(this).fadeOut(400, function(){
+									
+										that.data('position', addClass);
+										
+										that.removeClass('align-left');
+										that.removeClass('align-full');
+										that.removeClass('align-right');
+										
+										that.addClass('align-'+addClass);
+										
+										that.fadeIn(400);
+										isAnimating = false;
 									});
-								
-								   $('#content').animate({
-									height: pageHeight
-								  }, 0, function() {
-									// Animation complete.
-								  });
-								});
-					},
-					
-					drag:function(event, ui) {
-						//console.log($(this).css(event));
-						if (mousePos > 0 &&  mousePos < 475){
-							$(this).data('position', 'left');
-							$(this).addClass('align-left');
-							$(this).removeClass('align-full');
-							$(this).removeClass('align-right');
-							
-						}else if (mousePos > 475 &&  mousePos < 550){ 
-							$(this).data('position', 'full');
-							$(this).addClass('align-full');
-							$(this).removeClass('align-left');
-							$(this).removeClass('align-right');
-							
-						}else if (mousePos > 550 &&  mousePos < 1000){ 
-							$(this).data('position', 'right');
-							$(this).addClass('align-right');
-							$(this).removeClass('align-left');
-							$(this).removeClass('align-full');
-							
+								}
+							}
 						}
-						//console.log	(mousePos);
 					}
 					
-					
-				 });
+					//intialize functions
+					setNodeContainerHeight(); 
+					$('.node').draggable(nodeOptions);
+					initializeToolbox();
 				  
+					function setNodeContainerHeight() {
+						var pageHeight = 100;
 
-				  
-				  $('.save_btn').click(function() {
-					$('.node').each(function(index) {	
-						$(this).css("border","5px solid red");
-						var that = this;
-						
-						$.ajax({
-						  url: "thread/update_location/"+$(this).data('entry_id')+"/"+$(this).css('top')+"/"+$(this).data('position'),
-						  context: document.body,
-						  success: function(){
-							$(that).css("border","none");
-						  }
+						$('.node').each(function(index) {						
+							if ($(this).offset().top > pageHeight )
+								pageHeight = $(this).offset().top;
 						});
-					});
-				  
-				  });
 
-					$(document).mousemove(function(e){
-					
-						mousePos = e.pageX-$('#content').position().left;
+						$('#content-nodes').animate({
+						height: pageHeight
+						}, 0, function() {
+						// Animation complete.
+						});
+					}
+				
+
+					function initializeToolbox (){
+						$('.add_btn').click(function(){
+							var newNode = $('<div style="top: 195px; display: block;" data-position="left" class="node ui-draggable align-left"><article></article></div>');
+							$('#content-nodes').append(newNode);
+							newNode.draggable(nodeOptions);
+
+							return false;
+						});
 						
-						
-				   }); 
-				   
-					$( "#trash" ).droppable({
-						tolerance: 'pointer',
-						hoverClass: 'drophover',
-						over: function(event, ui) { 
-							$(ui.draggable).slideUp();
-						},
-						out: function(event, ui) { 
-							$(ui.draggable).slideDown();
-						},
-						drop: function( event, ui ) {
-							$( "#dialog-confirm" ).dialog({
-								resizable: false,
-								height:240,
-								width:540,
-								modal: true,
-								buttons: {
-									"Delete this entry": function() {
-									
-										$.ajax({
-										  url: "thread/delete_entry/"+$(ui.draggable).data('entry_id'),
-										  context: document.body,
-										  success: function(){
-											
-										  }
-										});
-										$( this ).dialog( "close" );
-									},
-									Cancel: function() {
-										$( this ).dialog( "close" );
+						$('.save_btn').click(function() {
+							$('.node').each(function(index) {	
+								$(this).css("border","5px solid red");
+								var that = this;
+
+								$.ajax({
+									url: "thread/update_location/"+$(this).data('entry_id')+"/"+$(this).css('top')+"/"+$(this).data('position'),
+									context: document.body,
+									success: function(){
+									$(that).css("border","none");
 									}
-								}
+								});
 							});
-						}
-					});
-								  
+						});
+						
+						$('#trash').droppable({
+							tolerance: 'pointer',
+							hoverClass: 'drophover',
+							over: function(event, ui) { 
+								$(ui.draggable).fadeOut();
+							},
+							out: function(event, ui) { 
+								$(ui.draggable).fadeIn();
+							},
+							drop: function( event, ui ) {
+								$( "#dialog-confirm" ).dialog({
+									resizable: false,
+									height:240,
+									width:540,
+									modal: true,
+									buttons: {
+										"Delete this entry": function() {
+										
+											$.ajax({
+											  url: "thread/delete_entry/"+$(ui.draggable).data('entry_id'),
+											  context: document.body,
+											  success: function(){
+												
+											  }
+											});
+											$( this ).dialog( "close" );
+										},
+										Cancel: function() {
+											$( this ).dialog( "close" );
+										}
+									}
+								});
+							}
+						});
+					}
+				 
+					$(document).mousemove(function(e){
+						mousePosX = e.pageX-$('#content-nodes').position().left;
+				    }); 			  
 				}
 				
 			</script>
 
-            <div id="content">
+            <div id="content-nodes">
 				<?php if($query): foreach($query as $post):?>
 				<div class = "node align-<?=$post->position;?>" data-entry_id = "<?=$post->entry_id;?>" data-position = "<?=$post->position;?>" style = "top:<?=$post->top;?>px">
 					<article>
@@ -171,10 +180,11 @@
 				<aside class = "insert">
 					<ul>
 						<li><a class = "save_btn" href="#">Save</a></li>
-						<li><a id="inline" href="#data" > <img src =  "<?php echo base_url() ?>assets/img/add-left.png"></a></li>
+						<li><a class = "add_btn" href="#" > <img src =  "<?php echo base_url() ?>assets/img/add-left.png"></a></li>
 						<li id="trash" class="ui-widget-header">
 							<p>Drop here</p>
 						</li>
+
 					</ul>
 				</aside>
 				
