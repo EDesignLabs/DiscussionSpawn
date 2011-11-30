6307b355f2cd0182804d1b5b77c3feec03e6d15d
Luke BrownGold <ma@gmail.com>
2011-11-29 22:58:11 -0500

    Text stats working(but not shown) in the user controller

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -20,11 +20,25 @@ class user extends MY_Controller {
 	//this function will retrive all entry in the database
 	public function profile($username)
 	{
+	
+		$this->load->library('TextStatistics');
+		//echo 
+		
 		//set page title
 		$data['title'] = "User";
 		$data['query'] = $this->user_model->get_user($username);
 		
 		$data['comments'] = $this->user_model->get_user_comments($username);
+		$comments = $data['comments'];
+		$complete_text = "";
+		
+		if($comments): foreach($comments as $comment):
+				$complete_text .= $comment->comment_body ;
+			endforeach;
+		endif; 
+		
+		$data['stats'] = $this->textstatistics->flesch_kincaid_reading_ease($complete_text);
+		
 		
 		$this->load->view('thread/user',$data);
 
