d1479cf99c75942b4548cfc0b0c4e929e4fc5edc
Luke BrownGold <ma@gmail.com>
2011-12-08 04:54:31 -0500

    Chrome bug fix

--- application/views/thread/index.php
+++ application/views/thread/index.php
@@ -11,17 +11,6 @@
 				<div class = "left section"></div>
 				<div class = "middle section"></div>
 				<div class = "right section"></div>
-			    
-
-				<div id="nodes">
-					<?php if($query): foreach($query as $post):?>
-					<div class = "node align-<?=$post->position;?>" data-entry_type = "<?=$post->entry_type;?>" data-entry_id = "<?=$post->entry_id;?>" data-status = "current" data-position = "<?=$post->position;?>" style = "top:<?=$post->top;?>px">
-						<? $this->load->view('modules/'.$post->entry_type , array('post' => $post)); ?>
-					</div>
-					<?php endforeach; else: ?>
-						<!--NO NODEs -->
-					<?php endif; ?>
-				</div><!-- Close nodes -->
 				
 				<?php if (in_array("can_edit_line", $this->tank_auth->get_permissions())): ?>
 					<aside id = "toolbox">
@@ -38,6 +27,18 @@
 						</ul>
 					</aside>
 				<?php endif; ?>
+
+				<div id="nodes">
+					<?php if($query): foreach($query as $post):?>
+					<div class = "node align-<?=$post->position;?>" data-entry_type = "<?=$post->entry_type;?>" data-entry_id = "<?=$post->entry_id;?>" data-status = "current" data-position = "<?=$post->position;?>" style = "top:<?=$post->top;?>px">
+						<? $this->load->view('modules/'.$post->entry_type , array('post' => $post)); ?>
+					</div>
+					<?php endforeach; else: ?>
+						<!--NO NODEs -->
+					<?php endif; ?>
+				</div><!-- Close nodes -->
+				
+
 			</div>
 			
 			<div id = "type-examples" style="display:none">				
