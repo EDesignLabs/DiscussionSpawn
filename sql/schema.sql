b1bd1a2b4ddb56edba56836f7307474ef710f178
Luke BrownGold <victorsigma@gmail.com>
2013-01-03 13:50:03 -0500

    Update sql/schema.sql
    Updated schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,13 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 3.2.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Nov 07, 2011 at 07:30 PM
--- Server version: 5.1.44
--- PHP Version: 5.3.1
+-- Use this schema for the database
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,7 +10,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `discussionspawn`
+-- Database: `discuss8_aster`
 --
 
 -- --------------------------------------------------------
@@ -25,6 +19,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `ci_sessions`
 --
 
+DROP TABLE IF EXISTS `ci_sessions`;
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
   `session_id` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '0',
   `ip_address` varchar(16) COLLATE utf8_bin NOT NULL DEFAULT '0',
@@ -32,12 +27,7 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
   `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
   `user_data` text COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`session_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `ci_sessions`
---
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -45,20 +35,41 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 -- Table structure for table `comment`
 --
 
+DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT,
   `entry_id` int(11) NOT NULL,
+  `comment_parent` int(11) NOT NULL,
   `comment_name` varchar(255) NOT NULL,
   `comment_email` varchar(255) NOT NULL,
   `comment_body` text NOT NULL,
   `comment_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`comment_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=67 ;
 
 --
 -- Dumping data for table `comment`
 --
 
+INSERT INTO `comment` (`comment_id`, `entry_id`, `comment_parent`, `comment_name`, `comment_email`, `comment_body`, `comment_date`) VALUES
+(49, 65, 40, 'admin', '', 'what', '2011-12-08 14:12:15'),
+(50, 65, 47, 'admin', '', 'this is a swweert', '2011-12-08 14:12:39'),
+(51, 65, 49, 'admin', '', 'cartman \r\n', '2011-12-08 14:13:11'),
+(52, 65, 48, 'admin', '', 'this is a reply to :what what', '2011-12-08 14:13:27'),
+(53, 65, 48, 'admin', '', 'hooters', '2011-12-08 14:20:04'),
+(54, 65, 53, 'admin', '', 'tester', '2011-12-08 14:23:21'),
+(55, 76, 0, 'teacher', '', 'asdffsd', '2013-01-03 18:13:45'),
+(56, 76, 0, 'teacher', '', 'asdffsd', '2013-01-03 18:13:53'),
+(57, 76, 0, 'teacher', '', 'asdffsd', '2013-01-03 18:16:25'),
+(58, 76, 0, 'teacher', '', 'asdffsd', '2013-01-03 18:17:54'),
+(59, 76, 0, 'teacher', '', 'hello', '2013-01-03 18:18:02'),
+(60, 76, 0, 'teacher', '', 'test', '2013-01-03 18:18:09'),
+(61, 76, 59, 'teacher', '', 'sfsdfdf', '2013-01-03 18:18:17'),
+(62, 62, 0, 'teacher', '', 'hello\n', '2013-01-03 18:25:32'),
+(63, 62, 62, 'teacher', '', 'hello from orage', '2013-01-03 18:25:59'),
+(64, 76, 60, 'teacher', '', 'good', '2013-01-03 18:33:27'),
+(65, 62, 63, 'teacher', '', 'bluebleue blue', '2013-01-03 18:41:09'),
+(66, 62, 39, 'teacher', '', 'factbook', '2013-01-03 18:41:30');
 
 -- --------------------------------------------------------
 
@@ -66,18 +77,19 @@ CREATE TABLE IF NOT EXISTS `comment` (
 -- Table structure for table `entry`
 --
 
+DROP TABLE IF EXISTS `entry`;
 CREATE TABLE IF NOT EXISTS `entry` (
   `entry_id` int(11) NOT NULL AUTO_INCREMENT,
-  `entry_name` varchar(255) NOT NULL,
-  `entry_body` text NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `field1` varchar(255) NOT NULL,
+  `field2` text NOT NULL,
+  `field3` varchar(255) NOT NULL,
   `entry_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `entry_type` varchar(255) NOT NULL,
+  `position` varchar(12) NOT NULL,
+  `top` int(11) NOT NULL,
   PRIMARY KEY (`entry_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `entry`
---
-
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=77 ;
 
 -- --------------------------------------------------------
 
@@ -85,18 +97,33 @@ CREATE TABLE IF NOT EXISTS `entry` (
 -- Table structure for table `login_attempts`
 --
 
+DROP TABLE IF EXISTS `login_attempts`;
 CREATE TABLE IF NOT EXISTS `login_attempts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(40) COLLATE utf8_bin NOT NULL,
   `login` varchar(50) COLLATE utf8_bin NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `login_attempts`
+-- Table structure for table `notifications`
 --
 
+DROP TABLE IF EXISTS `notifications`;
+CREATE TABLE IF NOT EXISTS `notifications` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'unread',
+  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -104,12 +131,14 @@ CREATE TABLE IF NOT EXISTS `login_attempts` (
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) COLLATE utf8_bin NOT NULL,
   `password` varchar(255) COLLATE utf8_bin NOT NULL,
   `email` varchar(100) COLLATE utf8_bin NOT NULL,
   `activated` tinyint(1) NOT NULL DEFAULT '1',
+  `permissions` varchar(255) COLLATE utf8_bin NOT NULL,
   `banned` tinyint(1) NOT NULL DEFAULT '0',
   `ban_reason` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `new_password_key` varchar(50) COLLATE utf8_bin DEFAULT NULL,
@@ -121,12 +150,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `users`
 --
 
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `activated`, `permissions`, `banned`, `ban_reason`, `new_password_key`, `new_password_requested`, `new_email`, `new_email_key`, `last_ip`, `last_login`, `created`, `modified`) VALUES
+(7, 'teacher', '$P$B5EAK8subrN1zQwJfkL5Y4d0.zb5Zo/', 'teacher@aol.com', 1, 'can_edit_line,can_edit_comments', 0, NULL, NULL, NULL, NULL, NULL, '74.9.76.166', '2013-01-03 11:48:03', '2013-01-03 11:45:05', '2013-01-03 18:48:03');
 
 -- --------------------------------------------------------
 
@@ -134,6 +165,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Table structure for table `user_autologin`
 --
 
+DROP TABLE IF EXISTS `user_autologin`;
 CREATE TABLE IF NOT EXISTS `user_autologin` (
   `key_id` char(32) COLLATE utf8_bin NOT NULL,
   `user_id` int(11) NOT NULL DEFAULT '0',
@@ -141,12 +173,7 @@ CREATE TABLE IF NOT EXISTS `user_autologin` (
   `last_ip` varchar(40) COLLATE utf8_bin NOT NULL,
   `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`key_id`,`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `user_autologin`
---
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -154,15 +181,23 @@ CREATE TABLE IF NOT EXISTS `user_autologin` (
 -- Table structure for table `user_profiles`
 --
 
+DROP TABLE IF EXISTS `user_profiles`;
 CREATE TABLE IF NOT EXISTS `user_profiles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_bin NOT NULL,
   `country` varchar(20) COLLATE utf8_bin DEFAULT NULL,
   `website` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `user_profiles`
 --
 
+INSERT INTO `user_profiles` (`id`, `user_id`, `username`, `country`, `website`) VALUES
+(7, 7, 'teacher', NULL, 'test');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
