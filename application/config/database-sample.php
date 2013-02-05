f1c7176832836554862b4f133bdf974dd784760e
Luke BrownGold <victorsigma@gmail.com>
2013-02-04 19:52:56 -0500

    Update application/config/database-sample.php

--- application/config/database-sample.php
+++ application/config/database-sample.php
@@ -42,8 +42,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['username'] = 'mysampleusername';
+$db['default']['password'] = 'mysamplepassword';
 $db['default']['database'] = 'thread';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
