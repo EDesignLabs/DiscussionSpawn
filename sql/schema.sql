06d81f889821a731780039437fd2f514f229db09
Luke BrownGold <victorsigma@gmail.com>
2013-02-04 20:05:21 -0500

    Update sql/schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,4 +1,11 @@
--- Use this schema for the database
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 04, 2013 at 06:04 PM
+-- Server version: 5.5.29
+-- PHP Version: 5.2.17
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -10,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `discuss8_aster`
+-- Database: `discuss8_lily`
 --
 
 -- --------------------------------------------------------
@@ -29,6 +36,14 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
   PRIMARY KEY (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Dumping data for table `ci_sessions`
+--
+
+INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
+('293aa5130c6ab9bd7c0251cd7fd8e182', '72.229.62.116', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17', 1360020060, 0x613a343a7b733a373a22757365725f6964223b733a313a2237223b733a383a22757365726e616d65223b733a373a2274656163686572223b733a31313a227065726d697373696f6e73223b613a323a7b693a303b733a31333a2263616e5f656469745f6c696e65223b693a313b733a31373a2263616e5f656469745f636f6d6d656e7473223b7d733a363a22737461747573223b733a313a2231223b7d),
+('b35c68eec1d34560384f2dad782539b0', '72.229.62.116', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17', 1360020722, '');
+
 -- --------------------------------------------------------
 
 --
@@ -45,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `comment_body` text NOT NULL,
   `comment_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`comment_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=67 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=69 ;
 
 --
 -- Dumping data for table `comment`
@@ -69,7 +84,8 @@ INSERT INTO `comment` (`comment_id`, `entry_id`, `comment_parent`, `comment_name
 (63, 62, 62, 'teacher', '', 'hello from orage', '2013-01-03 18:25:59'),
 (64, 76, 60, 'teacher', '', 'good', '2013-01-03 18:33:27'),
 (65, 62, 63, 'teacher', '', 'bluebleue blue', '2013-01-03 18:41:09'),
-(66, 62, 39, 'teacher', '', 'factbook', '2013-01-03 18:41:30');
+(66, 62, 39, 'teacher', '', 'factbook', '2013-01-03 18:41:30'),
+(67, 6, 0, 'teacher', '', ' hello ', '2013-01-14 03:19:26');
 
 -- --------------------------------------------------------
 
@@ -89,7 +105,20 @@ CREATE TABLE IF NOT EXISTS `entry` (
   `position` varchar(12) NOT NULL,
   `top` int(11) NOT NULL,
   PRIMARY KEY (`entry_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=77 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+
+--
+-- Dumping data for table `entry`
+--
+
+INSERT INTO `entry` (`entry_id`, `author_id`, `field1`, `field2`, `field3`, `entry_date`, `entry_type`, `position`, `top`) VALUES
+(5, 7, '', 'What is an example of opportunity cost from your own life? Think of a time when you had to make an important decision: What did you gain? What did you lose?', '', '2013-02-03 15:56:06', 'promptbox', 'full', 1410),
+(9, 7, 'Scarcity and Decision-Making', 'Re-read the passage we read yesterday on pg. 13. Then answer the following question: Why do Katniss and Gale decide to enter their names so many times?', 'true', '2013-02-04 14:17:46', 'title-textbox', 'full', 985),
+(10, 7, 'Making Connections', 'What is a real-world example of how people are forced to make tough decisions based on scarcity?', 'true', '2013-02-04 14:18:38', 'title-textbox', 'full', 1140),
+(11, 7, 'How do scarcity and opportunity cost influence characters in the Hunger Games?', '', 'false', '2013-02-04 18:37:20', 'title-textbox', 'full', 0),
+(13, 7, '511000f292693katniss_prim.jpg', 'I decided to volunteer for the Hunger Games instead of letting Prim go. What do you think of my decision? What was the opportunity cost of my decision?', 'true', '2013-02-04 18:42:36', 'imagebox', 'full', 595),
+(14, 7, '5110015ead7afkatniss.jpg', '', 'false', '2013-02-04 18:44:23', 'imagebox', 'full', 115),
+(15, 7, 'Take the poll! If you were Katniss, would you have volunteered for the Hunger Games? After selecting your answer, explain how opportunity cost influenced your decision.', '', 'Yes, No', '2013-02-04 18:47:41', 'poll', 'full', 495);
 
 -- --------------------------------------------------------
 
@@ -104,7 +133,7 @@ CREATE TABLE IF NOT EXISTS `login_attempts` (
   `login` varchar(50) COLLATE utf8_bin NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -123,7 +152,16 @@ CREATE TABLE IF NOT EXISTS `notifications` (
   `status` varchar(255) NOT NULL DEFAULT 'unread',
   `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `username`, `post_id`, `comment_id`, `message`, `type`, `status`, `insert_time`) VALUES
+(1, '0', 6, 0, 'Someone has replyed to "..."  !', 'reply', 'unread', '2013-01-14 03:19:26'),
+(2, '', 12, 0, 'Someone has replyed to "..."  !', 'reply', 'unread', '2013-01-24 18:37:24'),
+(3, '', 3, 0, 'Someone has replyed to "..."  !', 'reply', 'unread', '2013-01-29 18:17:20');
 
 -- --------------------------------------------------------
 
@@ -150,14 +188,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `activated`, `permissions`, `banned`, `ban_reason`, `new_password_key`, `new_password_requested`, `new_email`, `new_email_key`, `last_ip`, `last_login`, `created`, `modified`) VALUES
-(7, 'teacher', '$P$B5EAK8subrN1zQwJfkL5Y4d0.zb5Zo/', 'teacher@aol.com', 1, 'can_edit_line,can_edit_comments', 0, NULL, NULL, NULL, NULL, NULL, '74.9.76.166', '2013-01-03 11:48:03', '2013-01-03 11:45:05', '2013-01-03 18:48:03');
+(7, 'teacher', '$P$B5EAK8subrN1zQwJfkL5Y4d0.zb5Zo/', 'teacher@aol.com', 1, 'can_edit_line,can_edit_comments', 0, NULL, NULL, NULL, NULL, NULL, '72.229.62.116', '2013-02-04 16:23:51', '2013-01-03 11:45:05', '2013-02-04 23:23:51');
 
 -- --------------------------------------------------------
 
@@ -175,6 +213,13 @@ CREATE TABLE IF NOT EXISTS `user_autologin` (
   PRIMARY KEY (`key_id`,`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Dumping data for table `user_autologin`
+--
+
+INSERT INTO `user_autologin` (`key_id`, `user_id`, `user_agent`, `last_ip`, `last_login`) VALUES
+('b1c4d2bb96d22821581ed2b6a5456c39', 7, 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; MALC)', '65.219.220.130', '2013-01-30 17:50:38');
+
 -- --------------------------------------------------------
 
 --
@@ -189,14 +234,15 @@ CREATE TABLE IF NOT EXISTS `user_profiles` (
   `country` varchar(20) COLLATE utf8_bin DEFAULT NULL,
   `website` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `user_profiles`
 --
 
 INSERT INTO `user_profiles` (`id`, `user_id`, `username`, `country`, `website`) VALUES
-(7, 7, 'teacher', NULL, 'test');
+(7, 7, 'teacher', NULL, 'test'),
+(8, 8, 'HUMcharterstudent', NULL, 'test');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
